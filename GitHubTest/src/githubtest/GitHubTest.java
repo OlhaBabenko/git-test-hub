fcfebe72c0ee3e186f26686a4caa121ec2e51d65
OlhaBabenko <Olha_Babenko@epam.com>
2013-10-30 19:09:09 +0200

    Update GitHubTest.java
    change file in gitHub

--- GitHubTest/src/githubtest/GitHubTest.java
+++ GitHubTest/src/githubtest/GitHubTest.java
@@ -16,6 +16,7 @@ public class GitHubTest {
      */
     public static void main(String[] args) {
         System.out.println("Ta-da-dam!");
+        System.out.println("Change in GetHub");
     }
 
 }
